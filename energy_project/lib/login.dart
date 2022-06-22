6e50ed12c66ed57be5c9035f40477ffdb345b95b
Anusree-Anuzz <anusreek908@gmail.com>
2022-06-22 16:20:07 +0530

    Home Automation and Energy Monitoring

---
+++ energy_project/lib/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class screenLogin extends StatelessWidget {
+  const screenLogin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Login Screen"
+        ),
+      )
+    );
+  }
+}
